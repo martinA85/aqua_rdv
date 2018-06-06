eb5ff957a5107fc67e4ded37db6dd361016ed854
Martin Allimonier <allimonier.martin@gmail.com>
2018-06-06 11:05:45 +0200

    Adding valid_prestation function and button to the vieuw

--- models/event.py
+++ models/event.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models
 from datetime import datetime, timedelta
+from odoo.exceptions import Warning
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -17,3 +18,20 @@ class Meeting(models.Model):
         ('canceled', 'Annuler'),
         ('past', 'Fait'),
     ], default="valid")
+
+
+    def valid_prestation(self):
+        _logger.info("valid_prestation")
+        for meeting in self:
+            client_id = meeting.for_partner_id
+            type_prestation = meeting.prestation_id
+            card_id = self.env['aqua_rdv.card'].search([('client_id','=',client_id.id),('type_id','=',type_prestation.id)], order='validity asc',limit=1)
+            if card_id:
+                if card_id.number_actual > 0:
+                    card_id.number_actual = card_id.number_actual - 1
+                    meeting.state = "valid"
+                else:
+                    raise Warning(('''Forfait de la cliente vide'''))
+            else:
+                raise Warning(('''Pas de forfait : ''' + type_prestation.name))
+                    
